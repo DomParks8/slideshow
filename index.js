5c9c9be6c204b96ad1b87f0ef82f57a51c2241e1
Dominick <cowboysfan0888@gmail.com>
2020-04-28 23:46:55 -0400

    added play funtionality

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 var slideshow = {
-  photoList: ["photo1", "photo2", "photo3", "photo4", "photo5"],
-  currentPhotoIndex: 0,
-  nextPhoto: function () {
-    if (this.currentPhotoIndex == this.photoList.length - 1) {
-      console.log("End of Slideshow");
-    } else {
-      this.currentPhotoIndex++;
-      console.log(this.photoList[this.currentPhotoIndex]);
-    }
-  },
-  prevPhoto: function () {
-    if (this.currentPhotoIndex == 0) {
-      console.log("End of Slideshow");
-    } else {
-      this.currentPhotoIndex--;
-      console.log(this.photoList[this.currentPhotoIndex]);
-    }
-  },
-  getCurrentPhoto: function (currentPhotoIndex) {
-    return currentPhotoIndex;
-  },
+	photoList: ['photo1','photo2', 'photo3', 'photo4','photo5'],
+	currentPhotoIndex: 0,
+	nextPhoto: function(){
+	
+		if(this.currentPhotoIndex == (this.photoList.length-1)){
+
+			console.log("End of Slideshow");
+		} 
+		else{
+				this.currentPhotoIndex++;
+		console.log(this.photoList[this.currentPhotoIndex]);
+		}
+	},
+	prevPhoto: function(){
+	
+		if(this.currentPhotoIndex == 0){
+
+			console.log("End of Slideshow");
+		} 
+		else{
+				this.currentPhotoIndex--;
+		console.log(this.photoList[this.currentPhotoIndex]);
+		}
+	},
+	getCurrentPhoto: function(){
+		return this.photoList[currentPhotoIndex]; 
+
+	}
 };
+
+playInterval: null,
+
+    play: function() {
+        var self = this;
+        this.playInterval = setInterval(function(){
+        	self.nextPhoto()}, 2000)
+    },
+
+    pause: function() {
+        clearInterval(this.playInterval);
+    }
